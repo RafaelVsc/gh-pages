2010fc8926d3f5ccd7d3bd35de5bc6db13f4fe86
vescio <rafael.vescio@sensedata.com.br>
2020-01-13 21:14:05 -0300

    Updates

---
+++ precache-manifest.6a4f262a607c630f0fdb52942b4a5e9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d232202bba27019bdb69840c52ca387b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1ac1fe2ec5d502c2841",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "970b24e36f93d3eaabc1",
+    "url": "/static/js/2.28f8de5c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.28f8de5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1ac1fe2ec5d502c2841",
+    "url": "/static/js/main.0e0a94a6.chunk.js"
+  },
+  {
+    "revision": "f69c978edaca24fab18d",
+    "url": "/static/js/runtime-main.1992c66c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
